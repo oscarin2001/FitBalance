37d355497241c94c8c48e4b39d20d119ff2ca18c
Oscarin2001 <elmataperras392@gmail.com>
2025-11-25 15:10:28 -0400

    favicon y manifest

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -1,7 +1,29 @@
-import type { MetadataRoute } from 'next'
+import type { MetadataRoute } from "next";
 
 export default function manifest(): MetadataRoute.Manifest {
-    return {
-        "theme_color": "#8936FF", "background_color": "#2EC6FE", "icons": [{ "purpose": "maskable", "sizes": "512x512", "src": "icon512_maskable.png", "type": "image/png" }, { "purpose": "any", "sizes": "512x512", "src": "icon512_rounded.png", "type": "image/png" }], "orientation": "any", "display": "standalone", "dir": "auto", "lang": "en-US", "name": "FitBalance", "short_name": "fitbalance", "start_url": "/"
+  return {
+    theme_color: "#000000",
+  background_color: "#ffffff",
+  icons: [
+    {
+      purpose: "maskable",
+      sizes: "512x512",
+      src: "icon512_maskable.png",
+      type: "image/png"
+    },
+    {
+      purpose: "any",
+      sizes: "512x512",
+      src: "icon512_rounded.png",
+      type: "image/png"
     }
+  ],
+  orientation: "portrait",
+  display: "standalone",
+  dir: "auto",
+  lang: "es-ES",
+  start_url: "https://fit-balance-eight.vercel.app/dashboard",
+  name: "Fit Balance",
+  short_name: "FitBalance"
+  };
 }
